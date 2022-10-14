e272f673ce0e4f8d582e063aeee0a71916179f41
Arthur <arakhiyarov@gmail.com>
2022-10-14 18:04:01 +0500

    useCallback and useMemo hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import './App.css';
 import DemoOutput from './components/Demo/DemoOutput';
@@ -6,14 +6,22 @@ import Button from './components/UI/Button/Button';
 
 function App() {
     const [showParagraph, setshowParagraph] = useState(false);
+    const [allowToggle, setallowToggle] = useState(false);
+
     console.log('APP RUNNING!');
-    const toggleParagraphHandler = () => {
-        setshowParagraph((prevshowParagraph) => !prevshowParagraph);
+    const toggleParagraphHandler = useCallback(() => {
+        if (allowToggle) {
+            setshowParagraph((prevshowParagraph) => !prevshowParagraph);
+        }
+    }, [allowToggle]);
+    const allowToggleHandler = () => {
+        setallowToggle(true);
     };
     return (
         <div className="app">
             <h1>Hi there!</h1>
             <DemoOutput show={showParagraph} />
+            <Button onClick={allowToggleHandler}>Allow Toggling!</Button>
             <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
         </div>
     );
